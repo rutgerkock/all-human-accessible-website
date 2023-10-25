ca7dfb618762f95b1f06e3e95573c57d0d5482a8
rutgerkock <114153177+rutgerkock@users.noreply.github.com>
2023-10-25 16:04:04 +0200

    Added header_line

--- scripts/script.js
+++ scripts/script.js
@@ -40,3 +40,16 @@ document.addEventListener("DOMContentLoaded", function () {
         isInfoOpen = !isInfoOpen; 
     });
 });
+
+function updateHeaderLineValue() {
+    const headerLine = document.getElementById("header_line");
+    const maxScrollHeight = document.body.scrollHeight - window.innerHeight;
+    const currentScrollPosition = window.scrollY;
+    const percentageScrolled = (currentScrollPosition / maxScrollHeight) * 100;
+    
+    headerLine.value = percentageScrolled;
+    }
+    
+    window.addEventListener("scroll", updateHeaderLineValue);
+    
+    updateHeaderLineValue();
