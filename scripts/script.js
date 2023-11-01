fb865a523b2d02f3413a525566624cc95113ba52
rutgerkock <114153177+rutgerkock@users.noreply.github.com>
2023-11-01 09:59:56 +0100

    updates

--- scripts/script.js
+++ scripts/script.js
@@ -15,7 +15,7 @@ menuLinks.forEach(function (link) {
 
 function toggleMenu() {
     if (dropdownMenu.style.height === '0px' || dropdownMenu.style.height === '') {
-        dropdownMenu.style.height = '65vh';
+        dropdownMenu.style.height = '100vh';
         menuButton.classList.add('expanded');
         header.classList.add('header-expanded');
     } else {
@@ -24,3 +24,38 @@ function toggleMenu() {
         header.classList.remove('header-expanded');
     }
 }
+
+const meerLezenHiddenWaarom = document.getElementById('meer_lezen_hidden_waarom');
+const meerLezenWaarom = document.getElementById('meer_lezen_waarom');
+
+meerLezenWaarom.addEventListener('click', function () {
+    meerLezenHiddenWaarom.classList.toggle('show');
+});
+function showUpButton() {
+    var upButton = document.getElementById("up_button");
+    if (upButton) {
+        upButton.style.opacity = "1"; 
+        upButton.style.bottom = "20px"; 
+    }
+}
+
+function hideUpButton() {
+    var upButton = document.getElementById("up_button");
+    if (upButton) {
+        upButton.style.opacity = "0";
+        upButton.style.bottom = "0";
+    }
+}
+
+window.addEventListener("scroll", function() {
+    var mainSection = document.getElementById("waarom_section");
+    if (mainSection) {
+        if (window.scrollY > mainSection.offsetTop) {
+            showUpButton();
+        } else {
+            hideUpButton();
+        }
+    }
+});
+
+window.addEventListener("load", hideUpButton);
